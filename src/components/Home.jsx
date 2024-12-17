d38a2edad892427a34d8562d93e49c10a03d6d37
keshav <keshavardhan525@gmail.com>
2024-12-18 00:00:26 +0530

    first commit

---
+++ src/components/Home.jsx
@@ -0,0 +1,50 @@
+import React from "react";
+import Navbar from "./Navbar";
+import { useNavigate } from "react-router-dom";
+import { useState, useEffect } from "react";
+export const Home = () => {
+  const navigate = useNavigate();
+  const [Text, setText] = useState("");
+  const fullText = "Hello and Welcome!!!";
+  const speed = 50; //100 ms
+  const handle = () => {
+    navigate('/about');
+  }
+  useEffect(() => {
+    let index = 0;
+    const typeText = () => {
+      if (index < fullText.length) {
+        setText(fullText.substring(1, index + 1));
+        index++;
+        setTimeout(typeText, speed);
+      }
+    }
+    typeText();
+
+  }, []);
+  return (
+    <>
+      {/* Navbar */}
+      <Navbar />
+
+      {/* Main Content */}
+      <section className="py-5">
+        <div className="flex flex-col md:flex-row justify-center py-10 font-bold text-3xl">
+          <div className="md:w-1/2 text-center space-y-4 py-20">
+          <h1 className="">
+          <span className="text-red-600 ">
+                H
+              </span>
+            {Text}
+            </h1>
+            <div>This is keshavardhan</div>
+            <button className="h-10 text-xl" onClick={handle}> --- know more</button>
+          </div>
+          <div className="py-10 md:w-1/4 sm:w-1/2 flex justify-center">
+            <img className="rounded-2xl" src="/images/keshav.jpg" alt="img" />
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,57 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+
+const Navbar = () => {
+  return (
+    <>
+      <div className="container bg-black mx-auto flex justify-between items-center px-20 py-5 fixed w-full z-10 top-0 shadow-lg">
+        <span className="cursor-pointer">
+          <span className="text-red-600 font-bold hover:underline decoration-red-600 cursor-pointer">
+            K
+          </span>
+          eshav
+        </span>
+        <ul className="flex gap-5 font-bold ">
+          <li>
+            <NavLink
+              to="/"
+              className={({ isActive }) =>
+                isActive
+                  ? "underline decoration-red-600 font-bold"
+                  : "hover:underline decoration-red-600"
+              }
+            >
+              <span className="text-red-600">H</span>ome
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to="/about"
+              className={({ isActive }) =>
+                isActive
+                  ? "underline decoration-red-600 font-bold"
+                  : "hover:underline decoration-red-600"
+              }
+            >
+              <span className="text-red-600">A</span>bout
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to="/contact"
+              className={({ isActive }) =>
+                isActive
+                  ? "underline decoration-red-600 font-bold"
+                  : "hover:underline decoration-red-600"
+              }
+            >
+              <span className="text-red-600">C</span>ontact
+            </NavLink>
+          </li>
+        </ul>
+      </div>
+    </>
+  );
+};
+
+export default Navbar;
